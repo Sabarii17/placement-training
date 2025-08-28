2fb433047c379359b0aacacf4a71f903ab637996
Sabarii17 <sabariragul19@gmail.com>
2025-08-28 12:28:51 +0530

    Create String in the password format.java

---
+++ String in the password format.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner (System.in);
+        String st=s.nextLine();
+        int i=st.length();
+        for(int j=0;j<i;j++)
+        System.out.print("*");
+    }
+}

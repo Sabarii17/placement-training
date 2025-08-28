c1f2aad059b2ce58a4b529632e02643b4c4e3204
Sabarii17 <sabariragul19@gmail.com>
2025-08-28 12:31:59 +0530

    Create Reverse String.java

---
+++ Reverse String.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        String v=s.nextLine();
+        StringBuilder st=new StringBuilder(v);
+        st.reverse();
+        System.out.print(st);
+    }
+}

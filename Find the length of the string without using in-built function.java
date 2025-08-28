e71fc3e82e262bfc5408c1884b62ad0231395e3f
Sabarii17 <sabariragul19@gmail.com>
2025-08-28 12:24:50 +0530

    Create Find the length of the string without using in-built function.java

---
+++ Find the length of the string without using in-built function.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();int cn=0;
+       int i=0;try{
+        while(true){
+            char c=s.charAt(i);
+            cn++;
+            i++;
+        }}
+        catch(Exception e){
+           System.out.print(cn); 
+        }
+    }
+}

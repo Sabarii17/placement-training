b24eef14e2511e12c87be95c8e20cf876a851061
Sabarii17 <sabariragul19@gmail.com>
2025-08-28 12:33:53 +0530

    Create Sum of Digit.java

---
+++ Sum of Digit.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=a+b;
+        System.out.print("Sum of "+a+" and "+b+" is "+c);
+    }
+}
